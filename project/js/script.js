52e4051fca8872879ef6f736dfb6111203cff0e1
Giorgio Gristina <63541341+GiorgioGristina@users.noreply.github.com>
2020-07-14 14:15:11 +0200

    Update script.js

--- project/js/script.js
+++ project/js/script.js
@@ -1,1 +1,17 @@
 //$(".pho").lightbox();
+
+
+//declaring variable
+
+let image = $(".pho");
+let caption = $("data-title");
+const input = document.getElementByclass$(".search").value;
+// matching function
+
+function match(input){
+  if (input != caption){
+    image.hide()
+
+//  input = caption;
+//  match = true;
+}
