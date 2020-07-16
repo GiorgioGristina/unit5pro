d07ff93a4c0db36ab01d26354a6ad4cc573b74be
Giorgio Gristina <63541341+GiorgioGristina@users.noreply.github.com>
2020-07-16 10:49:47 +0200

    Update script.js

--- project/js/script.js
+++ project/js/script.js
@@ -3,15 +3,25 @@
 
 //declaring variable
 
-let image = $(".pho");
+let image = document.querySelectorAll(".pho");
 let caption = $("data-title");
-const input = document.getElementByclass$(".search").value;
+const input = document.getElementById("search");
+
 // matching function
 
-function match(input){
-  if (input != caption){
-    image.hide()
-
-//  input = caption;
-//  match = true;
+function match(){
+  const searchValue = input.value.toLowerCase();
+  console.log(searchValue);
+  for (let i = 0; i < image.length; i += 1) {
+    const image = image[i];
+    const caption = image.getAttribute("data-title").lowerCase();
+    const title = image.getAttribute("alt").lowerCase();
+    if (caption.indexOf(searchValue) > -1 || title.indexOf(searchValue) > -1){
+      image.style.display = "";
+    } else {
+      image.style.display = "none;"
+    }
+  }
 }
+
+input.addEventListener("keyup", match);
